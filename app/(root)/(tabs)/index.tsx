2fb059814aea025584b69de20ce4f6fd373046ae
Mohamed Sameh <hamadauter@gmail.com>
2024-12-25 13:30:18 +0300

    Initial commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { Link } from 'expo-router'
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-3xl my-10 font-rubik">Welcome to HouseHunter</Text>
+    </View>
+  );
+}
